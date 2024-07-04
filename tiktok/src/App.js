937feede3e03ed6168cb0a19a51c51c638e54b29
rishikkolli <rishikkolli@gmail.com>
2024-07-04 01:49:00 -0700

    Initial Setup for Landing Page

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import homeScreenImageOne from './images/home-screen-image-one.png';
 import './App.css';
 
 function App() {
   return (
     <div className="TiktokApp">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yo, this is a test. Edit <code>src/App.js</code> and save to reload. Cool.
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1>TikTok</h1>
+        <h1>HarmonicBytes</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Log In
         </a>
       </header>
+      <main>
+        <h1>Elevate your melodies and metric with HarmonicBytes</h1>
+        <img src={homeScreenImageOne} alt="Headphones unplugged playing melodious music." />
+      </main>
     </div>
   );
 }
